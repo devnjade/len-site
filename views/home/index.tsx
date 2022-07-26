d534ece0ae8427c980799f1b557d2a5621fa9ae5
Adebayo Temitope <adecancode@gmail.com>
2022-07-26 14:02:04 +0100

    update

---
+++ views/home/index.tsx
@@ -0,0 +1,13 @@
+import Layout from "layout";
+import React from "react";
+import styles from './index.module.scss';
+import Image from "next/image";
+
+const HomeView: React.FC = () => {
+  return (
+    <Layout header>
+    </Layout>
+  )
+}
+
+export default HomeView;
